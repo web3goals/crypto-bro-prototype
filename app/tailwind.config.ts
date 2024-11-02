0e870967f0e68a8ae7330a231d312626c02e5fd0
kiv1n <aloha@kiv1n.ru>
2024-11-02 12:51:49 +0300

    Fix fonts

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 const config: Config = {
   darkMode: ["class"],
   content: [
@@ -62,6 +64,9 @@ const config: Config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
