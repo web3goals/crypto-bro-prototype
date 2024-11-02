626f76b7f9df4fe6340af427bb39b8fa52bbd810
kiv1n <aloha@kiv1n.ru>
2024-11-02 12:35:03 +0300

    Improve project structure, add core components and configs

---
+++ app/components/site-header.tsx
@@ -0,0 +1,52 @@
+import { siteConfig } from "@/config/site";
+import { BicepsFlexedIcon, GithubIcon, MenuIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-40 bg-card border-b">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        {/* Left part */}
+        <div className="flex gap-6 md:gap-10">
+          <Link href="/" className="flex items-center space-x-2">
+            <BicepsFlexedIcon className="text-primary size-7" />
+            <span className="hidden text-primary font-bold md:inline">
+              {siteConfig.name}
+            </span>
+          </Link>
+        </div>
+        {/* Right part */}
+        <div className="flex flex-1 items-center justify-end gap-4">
+          <Link href="/chat">
+            <Button variant="default">Talk to Crypto Bro</Button>
+          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              className="text-sm font-medium text-muted-foreground"
+              asChild
+            >
+              <Button variant="ghost" size="icon">
+                <MenuIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <Link href={siteConfig.links.github} target="_blank">
+                <DropdownMenuItem>
+                  <GithubIcon className="mr-2 h-4 w-4" />
+                  <span>GitHub</span>
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ app/config/site.tsx
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Crypto Bro",
+  description:
+    "AI assistant to which you can delegate any of your blockchain requests, from token creation to participating in DAO",
+  links: {
+    github: "https://github.com/web3goals/crypto-bro-prototype",
+    twitter: "https://twitter.com/kiv1n",
+  },
+};
