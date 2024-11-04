8847bdfacd7af888f7cd930165ad8d674917d514
kiv1n <aloha@kiv1n.ru>
2024-11-04 19:09:23 +0300

    Implement features to get erc20 balance and transfer erc20 tokens

--- app/lib/chat.ts
+++ app/lib/chat.ts
@@ -3,8 +3,10 @@ import { cryptoBroAbi } from "@/contracts/abi/crypto-bro";
 import { rawTx } from "klaster-sdk";
 import OpenAI from "openai";
 import {
+  Address,
   createPublicClient,
   encodeFunctionData,
+  erc20Abi,
   http,
   parseEther,
   WalletClient,
@@ -56,6 +58,10 @@ async function processOpenAiToolCalls(
     if (toolCall.function.name === "get_wallet_eth_balance") {
       toolResponse = await getWalletEthBalance(walletClient);
     }
+    if (toolCall.function.name === "get_wallet_erc20_balance") {
+      const { erc20_address } = JSON.parse(toolCall.function.arguments);
+      toolResponse = await getWalletErc20Balance(erc20_address, walletClient);
+    }
     if (toolCall.function.name === "deploy_erc20_token") {
       const { token_name, token_symbol, token_initial_supply } = JSON.parse(
         toolCall.function.arguments
@@ -69,6 +75,17 @@ async function processOpenAiToolCalls(
     }
     if (toolCall.function.name === "get_deployed_erc20_tokens") {
       toolResponse = await getDeployedErc20Tokens(walletClient);
+    }
+    if (toolCall.function.name === "transfer_erc20_tokens") {
+      const { erc20_address, to, value } = JSON.parse(
+        toolCall.function.arguments
+      );
+      toolResponse = await transferErc20Tokens(
+        erc20_address,
+        to,
+        value,
+        walletClient
+      );
     }
     messages.push({
       role: "tool",
@@ -109,8 +126,34 @@ async function getWalletEthBalance(
     });
     return `${balance.toString()} ETH (${chainConfig.chain.name})`;
   } catch (error) {
-    console.error("Failed to get wallet address: ", errorToString(error));
-    return "Failed to get wallet address";
+    console.error("Failed to get wallet ETH balance: ", errorToString(error));
+    return "Failed to get wallet ETH balance";
+  }
+}
+
+async function getWalletErc20Balance(
+  erc20Address: string,
+  walletClient: WalletClient
+): Promise<string> {
+  try {
+    const client = createPublicClient({
+      chain: chainConfig.chain,
+      transport: http(chainConfig.chain.rpcUrls.default.http[0]),
+    });
+    const address = await getKlasterAccountAddress(
+      walletClient,
+      chainConfig.chain
+    );
+    const balance = await client.readContract({
+      address: erc20Address as Address,
+      abi: erc20Abi,
+      functionName: "balanceOf",
+      args: [address],
+    });
+    return `${balance.toString()} ERC20 (${chainConfig.chain.name})`;
+  } catch (error) {
+    console.error("Failed to get wallet ERC20 balance: ", errorToString(error));
+    return "Failed to get wallet ERC20 balance";
   }
 }
 
@@ -134,8 +177,8 @@ async function deployErc20Token(
     await executeKlasterRawTx(deployRawTx, walletClient, chainConfig.chain);
     return `ERC20 token deployed (${chainConfig.chain.name}), get the list of deployed token to see deployed smart contract address`;
   } catch (error) {
-    console.error("Failed to get wallet address: ", errorToString(error));
-    return "Failed to get wallet address";
+    console.error("Failed to deploy ERC20 token: ", errorToString(error));
+    return "Failed to deploy ERC20 token";
   }
 }
 
@@ -161,7 +204,35 @@ async function getDeployedErc20Tokens(
       (token) => `- ${token} (${chainConfig.chain.name})`
     )}`;
   } catch (error) {
-    console.error("Failed to get wallet address: ", errorToString(error));
-    return "Failed to get wallet address";
+    console.error(
+      "Failed to get deployed ERC20 tokens: ",
+      errorToString(error)
+    );
+    return "Failed to get deployed ERC20 tokens";
+  }
+}
+
+async function transferErc20Tokens(
+  erc20Address: string,
+  to: string,
+  value: string,
+  walletClient: WalletClient
+): Promise<string> {
+  try {
+    const transferFunctionData = encodeFunctionData({
+      abi: erc20Abi,
+      functionName: "transfer",
+      args: [to as Address, parseEther(value)],
+    });
+    const transferRawTx = rawTx({
+      gasLimit: 100_000n,
+      to: erc20Address as Address,
+      data: transferFunctionData,
+    });
+    await executeKlasterRawTx(transferRawTx, walletClient, chainConfig.chain);
+    return `ERC20 tokens transfered`;
+  } catch (error) {
+    console.error("Failed to transfer ERC20 tokens: ", errorToString(error));
+    return "Failed to transfer ERC20 tokens";
   }
 }
--- app/lib/openai.ts
+++ app/lib/openai.ts
@@ -21,6 +21,25 @@ export const OPEN_AI_TOOLS: OpenAI.Chat.ChatCompletionTool[] = [
   {
     type: "function",
     function: {
+      name: "get_wallet_erc20_balance",
+      description:
+        "Get user's crypto wallet token balance of specified ERC20 contract.",
+      parameters: {
+        type: "object",
+        properties: {
+          erc20_address: {
+            type: "string",
+            description: "Address of ERC20 contract.",
+          },
+        },
+        required: ["erc20_address"],
+        additionalProperties: false,
+      },
+    },
+  },
+  {
+    type: "function",
+    function: {
       name: "deploy_erc20_token",
       description: "Deploy ERC20 token.",
       parameters: {
@@ -53,14 +72,43 @@ export const OPEN_AI_TOOLS: OpenAI.Chat.ChatCompletionTool[] = [
         "Get the list of addresses of ERC20 tokens deployed by user.",
     },
   },
+  {
+    type: "function",
+    function: {
+      name: "transfer_erc20_tokens",
+      description:
+        "Transfer user's ERC20 tokens of specified contract to specified address.",
+      parameters: {
+        type: "object",
+        properties: {
+          erc20_address: {
+            type: "string",
+            description: "Address of ERC20 contract.",
+          },
+          to: {
+            type: "string",
+            description: "Recipient's address.",
+          },
+          value: {
+            type: "string",
+            description: "Amount of tokens to transfer.",
+          },
+        },
+        required: ["erc20_address"],
+        additionalProperties: false,
+      },
+    },
+  },
 ];
 
 export const OPEN_AI_SYSTEM_PROMPT = [
   "You are an assistant who helps users with various tasks on blockchain.",
-  "You can get user's wallet address, get user's wallet ETH balance, deploy ERC20 token, get deployed ERC20 token.",
+  "You can get user's wallet address, get user's wallet ETH balance, get user's wallet ERC20 balance, deploy ERC20 token, get deployed ERC20 token, transfer ERC20 tokens.",
   `You support only ${chainConfig.chain.name} blockchain.`,
   "Mention the blockchain name in your answers if it is possible.",
   "Your name is Crypto Bro. Talk with users like brothers.",
   "Suggest to users possible next actions.",
   "Be unique and interesting.",
+  "HINT: Vitalik Buterin's address is 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045.",
+  // "HINT: Test USDT address is 0x0000000000000000000000000000000000000000.", // TODO: Fill the address
 ].join(" ");
